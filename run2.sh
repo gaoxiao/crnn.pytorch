3e7e4475e8640582953574f99660b7352812445f
Xiao Gao <gaoxiao1988@gmail.com>
2019-05-24 15:09:41 -0700

    run2: IAM only + noise.

--- run2.sh
+++ run2.sh
@@ -2,12 +2,12 @@
 
 CUDA_VISIBLE_DEVICES=2 python train.py \
 --adadelta \
---trainRoot tool/train1 \
+--trainRoot tool/train \
 --valRoot tool/val \
 --cuda \
 --random_sample \
 --displayInterval 10 \
---valInterval 100 \
+--valInterval 300 \
 --batchSize 400 \
 --alphabet " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]_\`abcdefghijklmnopqrstuvwxyz~¡¢£©®°ÇÉ•€★" \
 --nepoch 200 \
@@ -15,4 +15,4 @@ CUDA_VISIBLE_DEVICES=2 python train.py \
 --use_aug \
 --use_noise \
 --pretrained /home/gaoxiao/code/crnn.pytorch/expr/IAM_2LSTM_nh512_mag8/0.77600.pth \
---trainName "IAM_GEN_512_valIAM_aug_noise"
+--trainName "IAM_512_valIAM_aug_noise"
