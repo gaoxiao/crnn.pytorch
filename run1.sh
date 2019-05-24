71845c95c1ed8b838b211d510feeafc67c9ed4cd
Xiao Gao <gaoxiao1988@gmail.com>
2019-05-24 14:59:16 -0700

    run1: 256

--- run1.sh
+++ run1.sh
@@ -7,11 +7,12 @@ CUDA_VISIBLE_DEVICES=1 python train.py \
 --cuda \
 --random_sample \
 --displayInterval 10 \
---valInterval 100 \
+--valInterval 300 \
 --batchSize 400 \
 --alphabet " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]_\`abcdefghijklmnopqrstuvwxyz~¡¢£©®°ÇÉ•€★" \
 --nepoch 200 \
---nh 512 \
+--nh 256 \
+--use_aug \
 --use_noise \
 --pretrained /home/gaoxiao/code/crnn.pytorch/expr/IAM_2LSTM_nh512_mag8/0.77600.pth \
---trainName "IAM_GEN_512_valIAM_noise"
+--trainName "IAM_GEN_256_valIAM_aug_noise"
